9f6a64dd21c86bd7e849f4596df7fd04ad3b6743
idanshturm <idanshturm1@gmail.com>
2021-07-17 12:13:42 -0500

    OurLastProject

--- PycharmProjects/SecureDevelopment/djangoSecureDeveleopment/CommunicationLTD/views.py
+++ PycharmProjects/SecureDevelopment/djangoSecureDeveleopment/CommunicationLTD/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'login.html')
+
+
+def sign(request):
+    return render(request, 'signup.html')
+
+def changePassword(request):
+    return render(request, 'change_password.html')
